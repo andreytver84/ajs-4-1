3c18cfb2daf9611ac8a83167944652aa7fb1d409
andreytver84 <uvikmedium@mail.ru>
2022-05-18 18:07:03 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// TODO: write your code here
+import sum from './basic';
+import healthCondition from './healthCondition';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
+
+console.log(healthCondition({name: 'Маг', health: 90}));
